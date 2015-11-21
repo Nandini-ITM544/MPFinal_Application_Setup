124b75bcba6e80ea52f05c17789ad1ba1f872f96
Nandini <nandini9590@gmail.com>
2015-11-21 13:58:28 -0600

    adding readonly details

--- gallery.php
+++ gallery.php
@@ -27,14 +27,14 @@ $client = RdsClient::factory([
 ]);
 
 $result = $client->describeDBInstances([
-    'DBInstanceIdentifier' => 'Project1db',
+    'DBInstanceIdentifier' => 'Project1readonly',
 ]);
 
 
 
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 //echo "begin database";
-$link = mysqli_connect($endpoint,"nandini","nandinipwd","Project1db") or die("Error " . mysqli_error($link));
+$link = mysqli_connect($endpoint,"nandini","nandinipwd","Project1readonly") or die("Error " . mysqli_error($link));
 
 /* check connection */
 if (mysqli_connect_errno()) {
