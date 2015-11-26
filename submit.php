bc16bd70b09cca7b758d49702af1300c6a0224d3
Nandini <nandini9590@gmail.com>
2015-11-25 20:01:10 -0600

    session

--- submit.php
+++ submit.php
@@ -7,11 +7,12 @@ echo $_POST['useremail'];
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 $fname = $_FILES['userfile']['name'];
+$_SESSION["email"]=$_POST['useremail'];
 echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "File is valid, and was successfully uploaded.\n";
 } else {
-    echo "Possible file upload attack!\n";
+    header("location: gallery.php");
 }
 echo 'Here is some more debugging info:';
 print_r($_FILES);
