f13147435a71f2a6634d457b332600bc3c5df606
Nandini <nandini9590@gmail.com>
2015-11-28 17:09:23 -0600

    adding

--- gallery.php
+++ gallery.php
@@ -54,7 +54,7 @@ $res = $link->use_result();
 
 while ($row = $res->fetch_assoc()) {
     echo " <li><img src =\" " . $row['raws3url'] . "\" /></li>";
-echo " <img src =\" " . $row['finisheds3url'] . "\" style="float:left;"/>";
+#echo " <img src =\" " . $row['finisheds3url'] . "\" style="float:left;"/>";
 }
 }
 else 
